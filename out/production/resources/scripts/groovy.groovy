6124a1980c7580c31a0701ff56a4144bf8505e53
Glom-c <glom@skillw.com>
2022-12-18 11:30:46 +0000

    sync version

--- out/production/resources/scripts/groovy.groovy
+++
@@ -1,5 +0,0 @@
-// When u use the Script Annotation, don't delete '//' before it.
-////@Listener(-event org.bukkit.event.player.PlayerChatEvent)
-//def hi() {
-//    event.player.sendMessage 'Hi! I\'m groovy.groovy::hi'
-//}
